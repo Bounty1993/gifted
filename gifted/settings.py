a3abbdea320b87ff0706bed0dfa7eab9eafa1261
Bartosz Niczyporuk <bartosz.niczyporuk781@gmail.com>
2019-06-25 19:07:55 +0200

    before adding celery

--- gifted/settings.py
+++ gifted/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 from decouple import config, Csv
 import dj_database_url
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -169,4 +169,4 @@ INTERNAL_IPS = ('127.0.0.1',)
 # required for channels
 ASGI_APPLICATION = 'gifted.routing.application'
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
